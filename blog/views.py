f24f20390a7aa8ab6fc5452bbef64747d17304b7
bawomg <bawo.mgemane@live.com>
2023-12-06 12:15:09 +0200

    Day 2 - Static files, Base HTML, CBV vs FBV

--- blog/views.py
+++ blog/views.py
@@ -4,3 +4,9 @@ from django.views.generic import TemplateView
 
 class HomeTemplateView(TemplateView):
     template_name = 'blog/home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Home'
+        context['body'] = 'This is the home page'
+        return context
